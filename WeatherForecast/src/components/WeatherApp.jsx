1df69e9c872580bc669b69c051ebb8e5ddedd441
Krishiv Saini <krishivsaini1506@gmail.com>
2025-05-02 19:40:55 +0530

    add Weather.css for styling the WeatherApp component

--- WeatherForecast/src/components/WeatherApp.jsx
+++ WeatherForecast/src/components/WeatherApp.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import './Weather.css'
 function WeatherApp() {
     const [data, setData] = useState({})
     const [Location, setLocation] = useState('')
