9c20e001c8fe77a946f03e654e5e31e4ac1e872a
Krishiv Saini <krishivsaini1506@gmail.com>
2025-06-10 17:24:50 +0530

    update react-router-dom to version 6.30.1 and refactor routing in App component

--- WeatherForecast/src/App.jsx
+++ WeatherForecast/src/App.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import WeatherApp from "./components/WeatherApp";
-import About from "./components/About";
-import Contact from "./components/Contact";
-// import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WeatherApp from "./components/WeatherApp.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import Login from './components/Login.jsx';
+import Signup from "./components/Signup.jsx";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<WeatherApp />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
